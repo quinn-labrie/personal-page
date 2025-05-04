51649207c594780c546dcef75fe58bc2033545a3
Quinn LaBrie <quinn@tronic.app>
2025-05-03 23:28:56 -0500

    progress, loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Card, CardContent, CardFooter } from './components/ui/card';
 import TypingText from './components/TypingText';
 import { info } from './lib/consts';
 import SocialLinks from './components/SocialLinks';
+import { Progress } from './components/ui/progress';
 
 function App() {
 	const [activeLineIndex, setActiveLineIndex] = useState(0);
+	const [progress, setProgress] = useState(0);
+	const [showContent, setShowContent] = useState(false);
+
+	useEffect(() => {
+		let startTime: number | null = null;
+
+		const duration = 1500;
+
+		const animate = (timestamp: number) => {
+			if (!startTime) startTime = timestamp;
+
+			const elapsed = timestamp - startTime;
+			const progressPercent = Math.min(elapsed / duration, 1);
+
+			const easedProgress = Math.round(
+				progressPercent *
+					progressPercent *
+					(3 - 2 * progressPercent) *
+					100
+			);
+
+			setProgress(easedProgress);
+
+			if (progressPercent < 1) {
+				requestAnimationFrame(animate);
+			} else if (progressPercent === 1) {
+				setTimeout(() => {
+					setShowContent(true);
+				}, 600);
+			}
+		};
+
+		requestAnimationFrame(animate);
+	}, []);
 
 	const handleLineComplete = () => {
 		setActiveLineIndex((prev) => {
@@ -21,7 +56,14 @@ function App() {
 		});
 	};
 
-	return (
+	return !showContent ? (
+		<Card className="items-start w-full max-w-md mx-auto mt-[10vh] card-appear">
+			<Progress
+				value={progress}
+				className={'my-3'}
+			/>
+		</Card>
+	) : (
 		<Card className="items-start w-full max-w-md mx-auto mt-[10vh] card-appear">
 			<CardContent
 				className="flex flex-col items-start"
